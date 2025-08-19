12c31e7af41c31540d36d0011399d0e1cab71898
amuhleth42 <96829299+amuhleth42@users.noreply.github.com>
2025-08-19 20:40:59 +0200

    Finish ft_write.s, errno handling done

--- lib/ft_write.s
+++ lib/ft_write.s
@@ -9,4 +9,14 @@ section .text
 	ft_write:
 		mov rax, 1
 		syscall
+		test rax, rax
+		js .error
+		ret
+
+	.error:
+		neg rax
+		mov rdx, rax
+		call __errno_location WRT ..plt
+		mov [rax], rdx
+		mov rax, -1
 		ret
