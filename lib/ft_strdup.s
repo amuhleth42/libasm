d1deb63d666f4fd48db6dac5e3554c53c2e4c21b
Aymeric Muhlethaler <amuhleth@c3r5s1.42lausanne.ch>
2025-08-19 17:59:32 +0200

    Debug ft_strdup.s

--- lib/ft_strdup.s
+++ lib/ft_strdup.s
@@ -5,29 +5,24 @@ extern ft_strcpy
 
 ; rdi <- const char *s
 
-section .text:
-
-ft_strdup:
-	.call_strlen:
-		push rdi
-		call ft_strlen
-	.call_malloc:
-		lea rdi, [rax + 1]
-		sub rsp, 8
-		call malloc WRT ..plt
-		add rsp, 8
-		cmp rax, 0
-		je .error
-	.call_strcpy:
-		mov rdi, rax
-		pop rsi
-		call ft_strcpy
-		ret
-
-
-	.error:
-		xor rax, rax
-		pop rdi
-		ret
+section .text
+	ft_strdup:
+		.call_strlen:
+			push rdi
+			call ft_strlen
+		.call_malloc:
+			lea rdi, [rax + 1]
+			call malloc WRT ..plt
+			cmp rax, 0
+			je .error
+		.call_strcpy:
+			mov rdi, rax
+			pop rsi
+			call ft_strcpy
+			ret
+		.error:
+			xor rax, rax
+			pop rdi
+			ret
 
 
