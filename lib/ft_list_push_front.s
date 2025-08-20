88b5543de88a69a242adfdeba608b46a2efd4534
amuhleth42 <96829299+amuhleth42@users.noreply.github.com>
2025-08-20 21:32:03 +0200

    Ft_list_push_front almost done

--- lib/ft_list_push_front.s
+++ lib/ft_list_push_front.s
@@ -1,4 +1,5 @@
 global	ft_list_push_front
+extern	ft_create_elem
 
 ; rdi <- t_list **begin_list
 ; rsi <- void *data
@@ -7,3 +8,32 @@ section	.text
 
 ft_list_push_front:
 
+	cmp rdi, 0
+	je .ret
+
+	mov rax, [rdi]
+	cmp rax, 0
+	jne .not_empty
+
+	push rdi
+	mov rdi, rsi
+	call ft_create_elem
+	pop rdx
+	mov [rdx], rax
+	ret
+
+.not_empty
+; to do
+	push rax
+	push rdi
+	mov rdi, rsi
+	call ft_create_elem
+	pop rdx
+	mov [rdx], rax
+	cmp rax, 0
+	je .ret
+	pop rcx
+	mov [rax], rcx
+
+.ret
+	ret
