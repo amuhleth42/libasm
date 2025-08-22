3c4cfd743967f6f916bb158eb638510a145e3fe7
Aymeric Muhlethaler <amuhleth@c3r3s5.42lausanne.ch>
2025-08-22 19:30:42 +0200

    Update ft_strdup.s

--- bonus/ft_strdup.s
+++ bonus/ft_strdup.s
@@ -7,26 +7,29 @@ extern __errno_location
 ; rdi <- const char *s
 
 section .text
-	ft_strdup:
-		.call_strlen:
-			push rdi
-			call ft_strlen
-		.call_malloc:
-			lea rdi, [rax + 1]
-			call malloc WRT ..plt
-			cmp rax, 0
-			je .error
-		.call_strcpy:
-			mov rdi, rax
-			pop rsi
-			call ft_strcpy
-			ret
-		.error:
+	
+ft_strdup:
+	.call_strlen:
+		push rdi
+		call ft_strlen
 
-			call __errno_location WRT ..plt
-			mov dword [rax], 12 ; code for ENOMEM
-			xor rax, rax
-			pop rdi
-			ret
+	.call_malloc:
+		lea rdi, [rax + 1]
+		call malloc WRT ..plt
+		cmp rax, 0
+		je .error
+
+	.call_strcpy:
+		mov rdi, rax
+		pop rsi
+		call ft_strcpy
+		ret
+
+	.error:
+		call __errno_location WRT ..plt
+		mov dword [rax], 12 ; code for ENOMEM
+		xor rax, rax
+		pop rdi
+		ret
 
 
