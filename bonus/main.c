8fa53ad4799ef01bf1dd7b9ecc767303f73dd4a3
Aymeric Muhlethaler <amuhleth@c3r3s5.42lausanne.ch>
2025-08-22 19:21:59 +0200

    Improve main.c

--- bonus/main.c
+++ bonus/main.c
@@ -20,31 +20,40 @@ void	print_list(t_list *lst)
 
 int main(void)
 {
-	char*	s;
+	char	*s;
 	s = "Salut mec\n";
 
-	printf("FT_WRITE\n");
+	printf("---------FT_STRLEN\n");
+	
+	printf("TEST 1 : \"Salut mec\"\n");
 	printf("Lib: %ld\n", write(1, s, strlen(s)));
 	printf("Cust: %ld\n", ft_write(1, s, strlen(s)));
 
-	
-	printf("FT_STRLEN\n");
+	s = "";
+	printf("TEST 2 : \"\"\n");
 	printf("Lib: %ld\n", strlen(s));
 	printf("Cust: %ld\n", ft_strlen(s));
 
-	printf("FT_STRCMP\n");
+	printf("\n----------FT_STRCMP\n");
+	printf("TEST 1 : salut, bonjour\n");
+	s = "salut";
 	char*	s2 = "bonjour\n";
 	printf("Lib: %d\n", strcmp(s, s2));
 	printf("Cust: %d\n", ft_strcmp(s, s2));
 	
+	s = "bonjour";
+	printf("TEST 2 : bonjour, bonjour\n");
+	printf("Lib: %d\n", strcmp(s, s2));
+	printf("Cust: %d\n", ft_strcmp(s, s2));
 	
-	printf("FT_STRCPY\n");
+	
+	printf("\n-----------FT_STRCPY\n");
 	char	dest1[25];
 	char	dest2[25];
 	printf("Lib: %s\n", strcpy(dest1, s));
 	printf("Cust: %s\n", ft_strcpy(dest2, s));
 
-	printf("FT_STRDUP\n");
+	printf("\n-----------FT_STRDUP\n");
 	char	*dup1 = strdup(s);
 	char	*dup2 = ft_strdup(s);
 	if (!dup1)
@@ -64,7 +73,7 @@ int main(void)
 	free(dup2);
 
 
-	printf("-------BONUS-------\n");
+	printf("\n-------BONUS-------\n");
 	t_list	*lst;
 	lst = NULL;
 
@@ -72,23 +81,18 @@ int main(void)
 	printf("ft_list_size: %d\n", ft_list_size(lst));
 	print_list(lst);
 
-	printf("TEST 2 : 1 elem: banana \n");
+	printf("\nTEST 2 : 1 elem: banana \n");
 	ft_list_push_front(&lst, "banana");
 	printf("ft_list_size: %d\n", ft_list_size(lst));
 	print_list(lst);
 
-	printf("TEST 3 : 3 elem \n");
-	printf("alive\n");
+	printf("\nTEST 3 : 3 elem \n");
 	ft_list_push_front(&lst, "apple");
-	printf("alive\n");
 	ft_list_push_front(&lst, "cherry");
-	printf("alive\n");
 	print_list(lst);
 	printf("ft_list_size: %d\n", ft_list_size(lst));
-	printf("alive\n");
-	print_list(lst);
 	
-	printf("TEST 4 : sorted \n");
+	printf("\nTEST 4 : sorted \n");
 	ft_list_sort(&lst, ft_strcmp);
 	print_list(lst);
 	return (0);
